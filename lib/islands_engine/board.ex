3917c6c9c39512a53b630017148439177efedec7
Michael Ries <michael@riesd.com>
2018-02-10 11:41:50 -0700

    guessing board locations

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -1,13 +1,52 @@
 defmodule IslandsEngine.Board do
-  alias IslandsEngine.Island
+  alias IslandsEngine.{Coordinate, Island}
 
   def new(), do: %{}
+
+  def guess(board, %Coordinate{} = coordinate) do
+    board
+    |> check_islands_for_hit(coordinate)
+    |> guess_response(board)
+  end
+
+  def all_islands_positioned?(board) do
+    Island.types() -- Map.keys(board) == []
+  end
 
   def position_island(board, key, %Island{}=island) do
     case overlaps_existing_island?(board, key, island) do
       true -> {:error, :overlapping_island}
       false -> Map.put(board, key, island)
     end
+  end
+
+  defp all_islands_forested?(board) do
+    Enum.all?(board, fn({_key, island}) ->
+      Island.forested?(island)
+    end)
+  end
+
+  defp check_islands_for_hit(board, coordinate) do
+    Enum.reduce(board, :miss, fn({key, island}, acc) ->
+      case Island.guess(island, coordinate) do
+        :miss -> acc
+        {:hit, updated_island} -> {:hit, key, updated_island}
+      end
+    end)
+  end
+
+  defp guess_response(:miss, board), do: {:miss, :none, :no_win, board}
+  defp guess_response({:hit, key, island}, board) do
+    board = Map.put(board, key, island)
+    win_or_no = case all_islands_forested?(board) do
+      true -> :win
+      false -> :no_win
+    end
+    forested_key = case Island.forested?(island) do
+      true -> key
+      false -> :none
+    end
+    {:hit, forested_key, win_or_no, board}
   end
 
   defp overlaps_existing_island?(board, new_key, new_island) do
--- test/board_test.exs
+++ test/board_test.exs
@@ -23,4 +23,41 @@ defmodule BoardTest do
     board = Board.new |> Board.position_island(:square, square)
     assert {:error, :overlapping_island} = Board.position_island(board, :dot, dot)
   end
+
+  test "it knows when all islands have been positioned" do
+    {:ok, atoll} = Island.new(:atoll, Coordinate.new(1, 1) |> elem(1))
+    {:ok, dot} = Island.new(:dot, Coordinate.new(2, 1) |> elem(1))
+    {:ok, l_shape} = Island.new(:square, Coordinate.new(5, 5) |> elem(1))
+    {:ok, s_shape} = Island.new(:dot, Coordinate.new(1, 3) |> elem(1))
+    {:ok, square} = Island.new(:square, Coordinate.new(8, 8) |> elem(1))
+    board = Board.new()
+    assert Board.all_islands_positioned?(board) == false
+    board = board |> Board.position_island(:atoll, atoll)
+    assert Board.all_islands_positioned?(board) == false
+    board = board |> Board.position_island(:dot, dot)
+    assert Board.all_islands_positioned?(board) == false
+    board = board |> Board.position_island(:l_shape, l_shape)
+    assert Board.all_islands_positioned?(board) == false
+    board = board |> Board.position_island(:s_shape, s_shape)
+    assert Board.all_islands_positioned?(board) == false
+    board = board |> Board.position_island(:square, square)
+    assert Board.all_islands_positioned?(board) == true
+  end
+
+  test "letting a user guess" do
+    {:ok, atoll} = Island.new(:atoll, Coordinate.new(1, 1) |> elem(1))
+    {:ok, dot} = Island.new(:dot, Coordinate.new(2, 1) |> elem(1))
+    {:ok, l_shape} = Island.new(:square, Coordinate.new(5, 5) |> elem(1))
+    {:ok, s_shape} = Island.new(:dot, Coordinate.new(1, 3) |> elem(1))
+    {:ok, square} = Island.new(:square, Coordinate.new(8, 8) |> elem(1))
+    board = Board.new()
+            |> Board.position_island(:atoll, atoll)
+            |> Board.position_island(:dot, dot)
+            |> Board.position_island(:l_shape, l_shape)
+            |> Board.position_island(:s_shape, s_shape)
+            |> Board.position_island(:square, square)
+    assert {:miss, :none, :no_win, ^board} = Board.guess(board, Coordinate.new(6, 1) |> elem(1))
+    assert {:hit, :dot, :no_win, board} = Board.guess(board, Coordinate.new(2,1) |> elem(1))
+    assert {:hit, :none, :no_win, _board} = Board.guess(board, Coordinate.new(1,1) |> elem(1))
+  end
 end
